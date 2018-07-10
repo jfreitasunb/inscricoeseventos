728d9f1be2797001e76f4261525af0e79c567143
jfreitasmat <jfreitas@mat.unb.br>
2018-07-10 15:14:40 -0300

    Seed de eventos.

---
+++ database/seeds/TipoEventoTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TipoEventoTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('tipo_evento')->delete();
+        
+        \DB::table('tipo_evento')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'tipo' => 'Workshop',
+                'created_at' => '2018-07-10 15:27:00',
+                'updated_at' => '2018-07-10 15:27:00',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'tipo' => 'Escola',
+                'created_at' => '2018-07-10 15:27:00',
+                'updated_at' => '2018-07-10 15:27:00',
+            ),
+        ));
+        
+        $tableToCheck = 'tipo_evento';
+
+        $highestId = DB::table($tableToCheck)->select(DB::raw('MAX(id)'))->first();
+        $nextId = DB::table($tableToCheck)->select(DB::raw('nextval(\''.$tableToCheck.'_id_seq\')'))->first();
+
+        DB::select('SELECT setval(\''.$tableToCheck.'_id_seq\', '.$highestId->max.')');
+        
+    }
+}
