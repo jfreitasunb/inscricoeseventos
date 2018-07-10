79c080469c17f9f92bcded7956de2b64533a2bbb
jfreitasmat <jfreitas@mat.unb.br>
2018-07-10 15:02:05 -0300

    Novos dados pessoais.

--- database/migrations/2018_07_10_010252_create_dados_pessoais_participante_table.php
+++ database/migrations/2018_07_10_010252_create_dados_pessoais_participante_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDadosPessoaisCandidatoTable extends Migration
+class CreateDadosPessoaisParticipanteTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateDadosPessoaisCandidatoTable extends Migration
      */
     public function up()
     {
-        Schema::create('dados_pessoais_candidato', function (Blueprint $table){
+        Schema::create('dados_pessoais_participante', function (Blueprint $table){
             $table->Increments('id');
-            $table->unsignedInteger('id_candidato');
-            $table->foreign('id_candidato')->references('id_user')->on('users')->onDelete('cascade');
-            $table->date('data_nascimento')->nullable();
+            $table->unsignedInteger('id_participante');
+            $table->foreign('id_participante')->references('id_user')->on('users')->onDelete('cascade');
+            $table->string('instituicao_recomendante',500)->nullable();
             $table->string('numerorg',30)->nullable();
             $table->string('endereco',255)->nullable();
             $table->string('cep',30)->nullable();
@@ -36,6 +36,6 @@ class CreateDadosPessoaisCandidatoTable extends Migration
      */
     public function down()
     {
-        Schema::drop('dados_pessoais_candidato');
+        Schema::drop('dados_pessoais_participante');
     }
 }
