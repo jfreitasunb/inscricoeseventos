5820cfbec45d52737356034d10f1a5449dfa0a1f
jfreitasmat <jfreitas@mat.unb.br>
2018-07-11 16:39:47 -0300

    Colunas da tabela com os dados do trabalho submetido.

--- database/migrations/2018_07_10_010260_create_trabalho_submetido_table.php
+++ database/migrations/2018_07_10_010260_create_trabalho_submetido_table.php
@@ -15,11 +15,15 @@ class CreateTipoParticipacaoTable extends Migration
     {
         Schema::create('tipo_participacao', function (Blueprint $table){
             $table->increments('id');
-            $table->unsignedInteger('id_categoria_participante');
-            $table->foreign('id_categoria_participante')->references('id')->on('categoria_participante')->onDelete('cascade');
-            $table->boolean('apresentar_trabalho');
-            $table->unsignedInteger('id_tipo_apresentacao')->nulable();
-            $table->foreign('id_tipo_apresentacao')->references('id')->on('tipo_apresentacao')->onDelete('cascade');
+            $table->unsignedInteger('id_participante');
+            $table->foreign('id_participante')->references('id_user')->on('users')->onDelete('cascade');
+            $table->unsignedInteger('id_area_trabalho');
+            $table->foreign('id_participante')->references('id_area_pos')->on('area_pos_mat')->onDelete('cascade');
+            $table->unsignedInteger('id_inscricao_evento');
+            $table->foreign('id_inscricao_evento')->references('id_inscricao_evento')->on('configura_inscricao_evento')->onDelete('cascade');
+            $table->text('titulo_trabalho');
+            $table->text('autor_trabalho');
+            $table->text('abstract_trabalho');
             $table->timestamps();
         });
     }
