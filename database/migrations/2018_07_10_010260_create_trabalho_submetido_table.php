d50bc89a1d6255469f7245d5a6324a7a901d4559
jfreitasmat <jfreitas@mat.unb.br>
2018-07-11 16:35:07 -0300

    Início da migration para registrar o trabalho submetido.

---
+++ database/migrations/2018_07_10_010260_create_trabalho_submetido_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipoParticipacaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_participacao', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('id_categoria_participante');
+            $table->foreign('id_categoria_participante')->references('id')->on('categoria_participante')->onDelete('cascade');
+            $table->boolean('apresentar_trabalho');
+            $table->unsignedInteger('id_tipo_apresentacao')->nulable();
+            $table->foreign('id_tipo_apresentacao')->references('id')->on('tipo_apresentacao')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tipo_participacao');
+    }
+}
