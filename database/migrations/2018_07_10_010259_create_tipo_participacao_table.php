851e3de74cc1969812b28879aa8fe527c288f997
jfreitasmat <jfreitas@mat.unb.br>
2018-07-11 16:34:10 -0300

    Nome da classe.

--- database/migrations/2018_07_10_010259_create_tipo_participacao_table.php
+++ database/migrations/2018_07_10_010259_create_tipo_participacao_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTipoApresentacaoTable extends Migration
+class CreateTipoParticipacaoTable extends Migration
 {
     /**
      * Run the migrations.
