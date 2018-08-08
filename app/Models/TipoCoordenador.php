81684383253b4fc7fa38ea9c5f2b2e20462903a9
jfreitasmat <jfreitas@mat.unb.br>
2018-08-08 17:28:01 -0300

    Model para criação de tipo de coordenador.

---
+++ app/Models/TipoCoordenador.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace InscricoesEventos\Models;
+
+use DB;
+use Carbon\Carbon;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TipoCoordenador extends Model
+{
+    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    use SoftDeletes;
+
+    protected $primaryKey = 'id';
+
+    protected $table = 'tipo_coordenador';
+
+    protected $fillable = [
+        'id_coordenador',
+        'coordenador_geral',
+        'coordenador_area',
+        'id_evento',
+        'deleted_at',
+    ];
+
+}
