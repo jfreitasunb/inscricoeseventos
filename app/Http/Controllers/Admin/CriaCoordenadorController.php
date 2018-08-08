a324be0710ecce60e4a3ec6257ec01ab2a8fcf6f
jfreitasmat <jfreitas@mat.unb.br>
2018-08-08 17:32:31 -0300

    Recuperando os dados para a view.

--- app/Http/Controllers/Admin/CriaCoordenadorController.php
+++ app/Http/Controllers/Admin/CriaCoordenadorController.php
@@ -27,38 +27,13 @@ use Illuminate\Pagination\LengthAwarePaginator;
 class CriaCoordenadorController extends AdminController
 {
 
-	public function getEditarInscricao()
+	public function getCriarCoordenador()
 	{
 
-		$edital = new ConfiguraInscricaoEvento();
+		$evento = new ConfiguraInscricaoEvento();
 
-      	$edital_vigente = $edital->retorna_edital_vigente();
+      	$evento_vigente = $evento->retorna_edital_vigente();
 
-      	return view('templates.partials.admin.editar_inscricao')->with(compact('edital_vigente'));
-	}
-
-	public function postEditarInscricao(Request $request)
-	{
-		$this->validate($request, [
-			'inicio_inscricao' => 'required|date_format:"Y-m-d"|before:fim_inscricao',
-			'fim_inscricao' => 'required|date_format:"Y-m-d"|after:inicio_inscricao',
-			'id_tipo_evento' => 'required',
-			'nome_evento' => 'required',
-			'ano_evento' => 'required',
-		]);
-
-		$edital_vigente = ConfiguraInscricaoEvento::find((int)$request->id_inscricao_evento);
-
-		$novos_dados_edital['inicio_inscricao'] = Purifier::clean(trim($request->inicio_inscricao));
-		$novos_dados_edital['fim_inscricao'] = Purifier::clean(trim($request->fim_inscricao));
-		$novos_dados_edital['id_tipo_evento'] = (int)Purifier::clean(trim($request->id_tipo_evento));
-		$novos_dados_edital['nome_evento'] = Purifier::clean(trim($request->nome_evento));
-		$novos_dados_edital['ano_evento'] = (int)Purifier::clean(trim($request->ano_evento));
-
-		$edital_vigente->update($novos_dados_edital);
-
-		notify()->flash('Inscrição alterada com sucesso!','success', ['timer' => 3000,]);
-
-		return redirect()->route('editar.inscricao');
+      	return view('templates.partials.admin.cria_coordenador')->with(compact('evento_vigente'));
 	}
 }
