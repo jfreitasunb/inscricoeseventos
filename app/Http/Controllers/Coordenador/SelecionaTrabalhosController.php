7aebe0db74b88a226b05fb911f95603a3fd99217
jfreitasmat <jfreitas@mat.unb.br>
2018-09-07 17:38:37 -0300

    Somente processa as seleções se o período estiver aberto.

--- app/Http/Controllers/Coordenador/SelecionaTrabalhosController.php
+++ app/Http/Controllers/Coordenador/SelecionaTrabalhosController.php
@@ -76,62 +76,68 @@ class SelecionaTrabalhosController extends CoordenadorController
 
         $relatorio_disponivel = $relatorio->retorna_edital_vigente();
 
-        $id_inscricao_evento = $relatorio_disponivel->id_inscricao_evento;
-
-        if ($request->id_inscricao_evento != $id_inscricao_evento) {
-            notify()->flash('Você está tentando selecionar trabalhos para evento não configurado!','error');
+        if ($relatorio_disponivel->selecao_trabalhos_finalizada) {
+            notify()->flash('O período de seleção de trabalhos já encerrou!','warning');
         
-            return redirect()->back();
-        }
+            return redirect()->route('home');
+        }else{
+            $id_inscricao_evento = $relatorio_disponivel->id_inscricao_evento;
 
-        if ($request->id_coordenador != $id_coordenador) {
-            notify()->flash('Você não tem permissão para executar essa operação!','error');
-        
-            return redirect()->back();
-        }
-        
-        $this->validate($request, [
-            'aceito' => 'required',
-        ]);
-// dd($request);
-        $trabalhos_aceitos = $request->aceito;
-        
-        $tipo_apresentacao = $request->muda_tipo_apresentacao;
-
-        $limpa_selecao = new TrabalhoSelecionado();
-
-        $limpa_selecao->limpa_selecoes_anteriores($id_coordenador, $id_inscricao_evento);
-
-        foreach ($trabalhos_aceitos as $key => $aceito) {
-            if ($aceito) {
-                
-                $selecionado = new TrabalhoSelecionado();
-
-                $tipo_participacao = new TipoParticipacao();
-
-                $submentido = new TrabalhoSubmetido();
-
-                $dados_submissao = $submentido->retorna_trabalho_submetido($key, $id_inscricao_evento);
-
-                $dados_participacao = $tipo_participacao->retorna_participacao($id_inscricao_evento, $key);
-
-                $id_categoria_participante = $dados_participacao->id_categoria_participante;
-                
-                $id_area_trabalho = $dados_submissao->id_area_trabalho;
-                
-                $selecionado->id_participante = $key;
-                $selecionado->id_categoria_participante = $id_categoria_participante;
-                $selecionado->id_tipo_apresentacao = $tipo_apresentacao[$key];
-                $selecionado->id_area_trabalho = $id_area_trabalho;
-                $selecionado->id_coordenador = $id_coordenador;
-                $selecionado->id_inscricao_evento = $id_inscricao_evento;
-
-                $selecionado->save();
+            if ($request->id_inscricao_evento != $id_inscricao_evento) {
+                notify()->flash('Você está tentando selecionar trabalhos para evento não configurado!','error');
+            
+                return redirect()->back();
             }
+
+            if ($request->id_coordenador != $id_coordenador) {
+                notify()->flash('Você não tem permissão para executar essa operação!','error');
+            
+                return redirect()->back();
+            }
+            
+            $this->validate($request, [
+                'aceito' => 'required',
+            ]);
+    // dd($request);
+            $trabalhos_aceitos = $request->aceito;
+            
+            $tipo_apresentacao = $request->muda_tipo_apresentacao;
+
+            $limpa_selecao = new TrabalhoSelecionado();
+
+            $limpa_selecao->limpa_selecoes_anteriores($id_coordenador, $id_inscricao_evento);
+
+            foreach ($trabalhos_aceitos as $key => $aceito) {
+                if ($aceito) {
+                    
+                    $selecionado = new TrabalhoSelecionado();
+
+                    $tipo_participacao = new TipoParticipacao();
+
+                    $submentido = new TrabalhoSubmetido();
+
+                    $dados_submissao = $submentido->retorna_trabalho_submetido($key, $id_inscricao_evento);
+
+                    $dados_participacao = $tipo_participacao->retorna_participacao($id_inscricao_evento, $key);
+
+                    $id_categoria_participante = $dados_participacao->id_categoria_participante;
+                    
+                    $id_area_trabalho = $dados_submissao->id_area_trabalho;
+                    
+                    $selecionado->id_participante = $key;
+                    $selecionado->id_categoria_participante = $id_categoria_participante;
+                    $selecionado->id_tipo_apresentacao = $tipo_apresentacao[$key];
+                    $selecionado->id_area_trabalho = $id_area_trabalho;
+                    $selecionado->id_coordenador = $id_coordenador;
+                    $selecionado->id_inscricao_evento = $id_inscricao_evento;
+
+                    $selecionado->save();
+                }
+            }
+            
+            notify()->flash('Dados salvos com sucesso!','success');
+            
+            return redirect()->back();
         }
-        
-        notify()->flash('Dados salvos com sucesso!','success');
-        
-        return redirect()->back();
     }
 }
