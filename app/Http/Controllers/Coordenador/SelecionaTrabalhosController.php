2fa6529c24eeaac87220c5d77cf4cc98ed2599e6
jfreitasmat <jfreitas@mat.unb.br>
2018-09-07 11:13:52 -0300

    Recuperando informações necessárias à visualização dos trabalhos submetidos com base no nível do coordenador.

--- app/Http/Controllers/Coordenador/SelecionaTrabalhosController.php
+++ app/Http/Controllers/Coordenador/SelecionaTrabalhosController.php
@@ -8,6 +8,26 @@ class SelecionaTrabalhosController extends CoordenadorController
 {
     public function getSelecionarTrabalhos()
     {
+        $user = $this->SetUser();
+    
+        $id_coordenador = $user->id_user;
+
+        $locale_relatorio = 'pt-br';
+
+        $relatorio = new ConfiguraInscricaoEvento();
+
+        $relatorio_disponivel = $relatorio->retorna_edital_vigente();
+
+        $id_inscricao_evento = $relatorio_disponivel->id_inscricao_evento;
+
+        $coordenador = new TipoCoordenador();
+
+        $nivel_coordenador = $coordenador->retorna_dados_coordenador($id_coordenador, $id_inscricao_evento);
+
+        $coordenador_area = $nivel_coordenador->coordenador_area;
+
+        $trabalho_submetido = new TrabalhoSubmetido();
+
         return view('templates.partials.coordenador.seleciona_trabalhos_submetidos');
     }
 
