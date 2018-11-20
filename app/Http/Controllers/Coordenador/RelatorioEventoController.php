f8636c233f6860de4b4f577a3a5ab2dd6450d6a8
jfreitasmat <jfreitas@mat.unb.br>
2018-11-20 09:01:12 -0200

    Controller para relatórios do evento.

---
+++ app/Http/Controllers/Coordenador/RelatorioEventoController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace InscricoesEventos\Http\Controllers\Coordenador;
+
+use Auth;
+use DB;
+use Mail;
+use Session;
+use File;
+use PDF;
+use Notification;
+use Carbon\Carbon;
+use InscricoesEventos\Models\User;
+use InscricoesEventos\Models\ConfiguraInscricaoEvento;
+use InscricoesEventos\Models\Formacao;
+use InscricoesEventos\Models\ProgramaPos;
+use InscricoesEventos\Models\FinalizaInscricao;
+use InscricoesEventos\Notifications\NotificaNovaInscricao;
+use Illuminate\Http\Request;
+use InscricoesEventos\Mail\EmailVerification;
+use InscricoesEventos\Http\Controllers\BaseController;
+use InscricoesEventos\Http\Controllers\CidadeController;
+use InscricoesEventos\Http\Controllers\AuthController;
+use InscricoesEventos\Http\Controllers\RelatorioController;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+
+/**
+* Classe para visualização da página inicial.
+*/
+class RelatorioEventoController extends CoordenadorController
+{
+
+	public function getGeraArquivosDiversos()
+	{
+		return view('templates.partials.coordenador.relatorio_pos');
+	}
+}
