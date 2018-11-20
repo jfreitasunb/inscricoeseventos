592742971a1e286b87ec4e6803fb608b8e51d08f
jfreitasmat <jfreitas@mat.unb.br>
2018-11-20 10:19:14 -0200

    Visualizando as opções de arquivos existentes.

--- resources/views/templates/partials/coordenador/relatorio_arquivos_diversos.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_arquivos_diversos.blade.php
@@ -5,7 +5,15 @@
   	<legend>Selecione abaixo qual(is) arquivo(s) deseja gerar:</legend>
   	{!! Form::open(array('route' => 'relatorio.arquivos.diversos','data-parsley-validate' => '')) !!}
 
-
+  	@foreach($tipo_de_arquivo_disponivel as $arquivo_gerado => $key)
+            <div class="col-xs-6">
+              <div class="form-group form-inline">
+                <label>
+                  {!! Form::checkbox('arquivos_para_gerar[]', $arquivo_gerado, null) !!} {{ $key }} 
+                </label> 
+              </div>
+            </div>
+          @endforeach
   	<div class="col-md-10 text-center"> 
         {!! Form::submit('Gerar Arquivos', array('class' => 'register-submit btn btn-primary btn-lg', 'id' => 'register-submit', 'tabindex' => '4')) !!}
 	</div>
