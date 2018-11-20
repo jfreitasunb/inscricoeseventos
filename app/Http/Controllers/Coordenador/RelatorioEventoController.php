a15b3491411ae6df9d1c1595a7f3bc3fec43f9a5
jfreitasmat <jfreitas@mat.unb.br>
2018-11-20 14:28:48 -0200

    Exibe opção de relatório se já houve algum trabalho selecionado.

--- app/Http/Controllers/Coordenador/RelatorioEventoController.php
+++ app/Http/Controllers/Coordenador/RelatorioEventoController.php
@@ -13,6 +13,7 @@ use Carbon\Carbon;
 use InscricoesEventos\Models\User;
 use InscricoesEventos\Models\ConfiguraInscricaoEvento;
 use InscricoesEventos\Models\FinalizaInscricao;
+use InscricoesEventos\Models\TrabalhoSelecionado;
 use InscricoesEventos\Notifications\NotificaNovaInscricao;
 use Illuminate\Http\Request;
 use InscricoesEventos\Mail\EmailVerification;
@@ -41,14 +42,20 @@ class RelatorioEventoController extends CoordenadorController
 	    $relatorio_disponivel = $relatorio->retorna_edital_vigente();
 
 	    $id_inscricao_evento = $relatorio_disponivel->id_inscricao_evento;
-	    
+
+	    $trabalhos_selecionados = new TrabalhoSelecionado();
+
+	    $existe_selecao = $trabalhos_selecionados->existe_trabalho_selecionado($id_inscricao_evento);
+
 		$tipo_de_arquivo_disponivel['cracha'] = "Arquivos para gerar os crachás";
 		
 		$tipo_de_arquivo_disponivel['lista_participante'] = "Arquivos com a lista de todos os participantes inscritos";
 		
 		$tipo_de_arquivo_disponivel['lista_trabalhos_submetidos'] = "Arquivos com a lista de trabalhos submetidos";
 
-		$tipo_de_arquivo_disponivel['lista_trabalhos_aceitos'] = "Arquivos com a lista dos trabalhos aceitos";
+		if ($existe_selecao) {
+			$tipo_de_arquivo_disponivel['lista_trabalhos_aceitos'] = "Arquivos com a lista dos trabalhos aceitos";
+		}
 
 		return view('templates.partials.coordenador.relatorio_arquivos_diversos')->with(compact('tipo_de_arquivo_disponivel'));
 	}
