77966f3082a986db3210b64d946012cf6d965c2e
jfreitasmat <jfreitas@mat.unb.br>
2018-12-11 16:00:41 -0200

    MIgration para configurar os detalhes do evento.

---
+++ database/migrations/2018_12_11_010253_create_configura_detalhes_evento_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfiguraDetalhesEventoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configura_detalhes_evento', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('id_inscricao_evento');
+            $table->foreign('id_inscricao_evento')->references('id_inscricao_evento')->on('configura_inscricao_evento')->onDelete('cascade');
+            $table->boolean('finalizada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('configura_detalhes_evento');
+    }
+}
