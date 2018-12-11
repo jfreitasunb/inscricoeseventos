4f418d41252c36778ce6988d0b38e7afa9c5acf8
jfreitasmat <jfreitas@mat.unb.br>
2018-12-11 17:45:30 -0200

    Array de meses.

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -16,6 +16,20 @@ use View;
 
 class BaseController extends Controller
 {
+    protected $array_meses  = array(
+                '1' => 'Janeiro',
+                '2' => 'Fevereiro',
+                '3' => 'Março',
+                '4' => 'Abril',
+                '5' => 'Maio',
+                '6' => 'Junho',
+                '7' => 'Julho',
+                '8' => 'Agosto',
+                '9' => 'Setembro',
+                '10' => 'Outubro',
+                '11' => 'Novembro',
+                '12' => 'Dezembro',
+              );
 
 	public $periodo_inscricao;
 
