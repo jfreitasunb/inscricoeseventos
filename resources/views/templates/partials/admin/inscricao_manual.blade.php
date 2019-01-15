04ddf7f5796f21b986cfb7127eaee684992600ca
jfreitasmat <jfreitas@mat.unb.br>
2019-01-15 14:50:14 -0200

    Dados pessoais do participante.

--- resources/views/templates/partials/admin/inscricao_manual.blade.php
+++ resources/views/templates/partials/admin/inscricao_manual.blade.php
@@ -9,36 +9,70 @@
 @section('inscricao_manual')
 
 <fieldset class="scheduler-border">
-  <legend class="scheduler-border">Inscrições não finalizadas</legend>
+  <legend class="scheduler-border">Realizar inscrição manualmente</legend>
 
-  <div class="table-responsive">
-    <table class="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Nome do participante</th>
-          <th scope="col">Deseja apresentar trabalho</th>
-          <th scope="col">Finalizar Inscrição?</th>
-        </tr>
-      </thead>
-      <tbody>
-        {!! Form::open(array('route' => 'inscricoes.com.problemas', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
-        {!! Form::hidden('id_inscricao_evento', $id_inscricao_evento, []) !!}
-        @foreach( $contas_para_finalizar as $key => $nao_finalizada)
-          <tr>
-            <td>{{ $nao_finalizada['nome'] }}</td>
-            <td>{{ $nao_finalizada['apresentar_trabalho'] }}</td>
-            <td><input type="radio" name="finalizar_manualmente[{{ $key }}]" value=1>Sim <input type="radio" name="finalizar_manualmente[{{ $key }}]" value="0" checked>Não</td>
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div>
-  <div class="form-group">
   <div class="row">
-    <div class="col-md-6 col-md-offset-3 text-center">
-      {!! Form::submit('Finalizar Inscrição', ['class' => 'btn btn-primary btn-lg register-submit']) !!}
-    </div>
-  </div>
+  
+    {!! Form::open(array('route' => 'inscricoes.manual', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+
+        
+        
+          <div class="row">
+            {!! Form::label('nome', trans('tela_dados_pessoais.nome'), ['class' => 'col-md-4 control-label'])!!}
+            <div class="col-md-4">
+              {!! Form::text('nome', '', ['class' => 'form-control input-md formhorizontal', 'required' => '']) !!}
+            </div>
+          </div>
+        
+
+        
+          <div class="row">
+            {!! Form::label('nome_cracha', trans('tela_dados_pessoais.nome_cracha'), ['class' => 'col-md-4 control-label'])!!}
+            <div class="col-md-4">
+              {!! Form::text('nome_cracha', '' , ['class' => 'form-control input-md formhorizontal', 'required' => '']) !!}
+            </div>
+          </div>
+        
+
+        
+          <div class="row">
+          {!! Form::label('numero_documento', trans('tela_dados_pessoais.numero_documento'), ['class' => 'col-md-4 control-label'])!!}
+          <div class="col-md-4">
+            {!! Form::text('numero_documento', '' , ['class' => 'form-control input-md formhorizontal', 'required' => '']) !!}
+          </div>
+        </div>
+        
+        
+
+        
+          <div class="row">
+          {!! Form::label('instituicao', trans('tela_dados_pessoais.instituicao'), ['class' => 'col-md-4 control-label'])!!}
+          <div class="col-md-4">
+            {!! Form::text('instituicao', '' , ['class' => 'form-control input-md formhorizontal', 'required' => '']) !!}
+          </div>
+        </div>
+        
+        
+        
+          <div class="row">
+          {!! Form::label('id_pais', trans('tela_dados_pessoais.pais'), ['class' => 'col-md-4 control-label'])!!}
+          <div class="col-md-4">
+            {!! Form::select('id_pais', ['' => 'Select'] +$countries,'',array('class'=>'form-control input-md formhorizontal','id'=>'pais'));!!}
+          </div>
+        </div>
+        
+        
+        
+          <div class="form-group">
+            <div class="row">
+              <div class="col-md-6 col-md-offset-3 text-center">
+                {!! Form::submit(trans('tela_dados_pessoais.menu_enviar'), ['class' => 'btn btn-primary btn-lg register-submit']) !!}
+              </div>
+            </div>
+          </div>
+        
+      {!! Form::close() !!}
+    </fieldset>
 </div>
 </fieldset>
 
